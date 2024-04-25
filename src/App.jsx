6cc055f47058a1db2f5607fa570b331f49f580c5
guzungrigore <113361340+guzungrigore@users.noreply.github.com>
2024-04-25 12:07:56 +0300

    Add toggle theme, navigation to country

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,58 @@
 import "./App.css"
-import {createContext} from "react";
+import {createContext, useState} from "react";
 import {useCountries} from "./hooks/useCountries.js";
 import {CountriesPage} from "./components/CountriesPage.jsx";
 import {CountryPage} from "./components/CountryPage.jsx";
 import {Route, Routes} from "react-router-dom";
+import {ThemeIcon} from "./components/themeIcon/themeIcon.jsx";
+
 export const CountryContext = createContext(null);
 
 function App() {
     const {isLoading, countries, errorMessage} = useCountries("https://restcountries.com/v3.1/all")
 
-  return (
-      <div className="App">
-          <CountryContext.Provider
-              value={{isLoading, countries, errorMessage}}>
-              <Routes>
-                  <Route path={"/"} element={<CountriesPage/>}/>
-                  <Route path={"/country/:name"} element={<CountryPage/>}/>
-              </Routes>
-          </CountryContext.Provider>
-      </div>
-  )
+    const getMediaPreference = () => {
+        const hasDarkPreference = window.matchMedia(
+            "(prefers-color-scheme: dark)"
+        ).matches;
+        if (hasDarkPreference) {
+            return "dark-theme";
+        } else {
+            return "light-theme";
+        }
+    }
+    const [userTheme, setUserTheme] = useState(localStorage.getItem("user-theme") || getMediaPreference());
+    document.documentElement.className = userTheme;
+
+    const setTheme = (theme) => {
+        localStorage.setItem("user-theme", theme);
+        setUserTheme(theme)
+        document.documentElement.className = theme;
+    }
+
+    const toggleTheme = () => {
+        const activeTheme = localStorage.getItem("user-theme");
+        console.log(activeTheme)
+        if (activeTheme === "light-theme") {
+            setTheme("dark-theme");
+        } else {
+            setTheme("light-theme");
+        }
+    }
+    return (
+        <div className="App">
+            <CountryContext.Provider
+                value={{isLoading, countries, errorMessage}}>
+                <div className={"settings"}>
+                    <ThemeIcon toggleTheme={toggleTheme} userTheme={userTheme}/>
+                </div>
+                <Routes>
+                    <Route path={"/"} element={<CountriesPage/>}/>
+                    <Route path={"/country/:name"} element={<CountryPage/>}/>
+                </Routes>
+            </CountryContext.Provider>
+        </div>
+    )
 }
 
 export default App
--- src/components/CountryPage.jsx
+++ src/components/CountryPage.jsx
@@ -1,7 +1,60 @@
+import {Link, useParams} from "react-router-dom";
+import {useCountries} from "../hooks/useCountries.js";
+
 export const CountryPage = () => {
+    const {name} = useParams()
+    const {isLoading, countries, errorMessage} = useCountries(`https://restcountries.com/v3.1/name/${name}`)
+    const country = countries && countries[0]
     return (
         <div>
-            <p>country</p>
+
+
+            <div>{isLoading && <p>Is loading...</p>}</div>
+            <div>{errorMessage && <p>{errorMessage}</p>}</div>
+            {country && (
+                <div className={"single-country"}>
+                    <div className={"single-country-nav"}>
+                        <Link to={"/"} className={'back'}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                 strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                                 className="feather feather-arrow-left icon">
+                                <line x1="19" y1="12" x2="5" y2="12"></line>
+                                <polyline points="12 19 5 12 12 5"></polyline>
+                            </svg>
+                        </Link>
+                    </div>
+                    <h1 className={"single-country-name"}>{country.name.common}</h1>
+                    <div className={"single-country-information"}>
+                        <div className={"single-country-info"}>
+                            <p>Official Name: </p>
+                            <h3>{country.name.official}</h3>
+                        </div>
+                        <div className={"single-country-info"}>
+                            <p>Capital: </p>
+                            <h3>{country.capital}</h3>
+                        </div>
+                        <div className={"single-country-info"}>
+                            <p>Region: </p>
+                            <h3>{country.region}</h3>
+                        </div>
+                        <div className={"single-country-info"}>
+                            <p>Population: </p>
+                            <h3>{country.population} People</h3>
+                        </div>
+                        <div className={"single-country-info"}>
+                            <p>Native Language: </p>
+                            <h3>{country.languages[Object.keys(country.languages)[0]]}</h3>
+                        </div>
+                    </div>
+                    <img className={"img"} src={country.flags.png}/>
+
+                    <div className={"single-country-info-last"}>
+                        <p>Location: </p>
+                        <a href={country.maps.googleMaps}>{country.maps.googleMaps}</a>
+                    </div>
+                </div>
+            )
+            }
         </div>
     )
 }
